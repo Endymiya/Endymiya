0cc9b2d262c25c77e43c139c54b3267353516f86
Ceam <ceam@ceam.sdsds.br>
2019-09-26 18:28:12 -0300

    bhaskara

--- ex1/script.js
+++ ex1/script.js
@@ -10,15 +10,22 @@ window.addEventListener('load', function(event) {
 
 
 function processar() {
-    console.log('vamos começar a processar!');
-
+    console.log('Digite A , B e C')
     let valorA = parseFloat(document.querySelector('#inputA').value);
     let valorB = parseFloat(document.querySelector('#inputB').value);
+    let valorC = parseFloat(document.querySelector('#inputC').value);
 
-    let soma = valorA + valorB;
+    let delta = (valorB*valorB)-4*(valorA*valorC);
+    let cima1 = -valorB + Math.sqrt(delta);
+    let cima2 = -valorB - Math.sqrt(delta);
+    let valorX1 = cima1/(2*valorA);
+    let valorX2 = cima2/(2*valorA);
 
-    document.querySelector('#resultado').innerHTML = soma;
-    console.log('O resultado é: ', soma);
+
+    document.querySelector('#resultado').innerHTML = valorX1;
+    console.log('O resultado é: ', valorX1);
+    document.querySelector('#resultado2').innerHTML = valorX2;
+    console.log('O resultado é: ', valorX2);
 }
 
 
